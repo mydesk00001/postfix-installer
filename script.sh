7619244c97e2762978028116d34ef3472cef4880
mydesk00001 <114636614+mydesk00001@users.noreply.github.com>
2023-02-11 18:28:46 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,51 +1,41 @@
 #!/bin/bash
 
-# Install postfix
+# Update the package list
 sudo apt-get update
-sudo apt-get install postfix -y
 
-# Install OpenSSL
-sudo apt-get install openssl -y
+# Install Postfix and OpenSSL
+sudo apt-get install postfix openssl
 
-# Prompt for hostname, mail user, and password
-read -p "Enter hostname: " hostname
-read -p "Enter mail user: " mailuser
-read -p "Enter password: " password
+# Prompt the user for the email address, password, and hostname
+read -p "Enter your email address: " email
+read -sp "Enter your email password: " password
+echo
+read -p "Enter the hostname for your server: " hostname
 
-# Generate SSL certificate using OpenSSL
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/$hostname.key -out /etc/ssl/certs/$hostname.crt \
--subj "/C=US/ST=CA/L=San Francisco/O=Company Name/OU=Org/CN=$hostname"
+# Generate a self-signed SSL certificate
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/postfix.key -out /etc/ssl/certs/postfix.crt -subj "/CN=$hostname"
 
-# Set permissions on SSL files
-sudo chmod 600 /etc/ssl/private/$hostname.key
-sudo chmod 644 /etc/ssl/certs/$hostname.crt
-
-# Set postfix configurations
-sudo postconf -e "myhostname = $hostname"
-sudo postconf -e "smtpd_banner = "
-sudo postconf -e "smtpd_relay_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination"
-sudo postconf -e "smtpd_recipient_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination"
-sudo postconf -e "smtpd_sasl_auth_enable = yes"
-sudo postconf -e "smtpd_sasl_authenticated_header = yes"
-sudo postconf -e "smtpd_sasl_local_domain ="
-sudo postconf -e "smtpd_sasl_security_options = noanonymous"
-sudo postconf -e "smtpd_tls_auth_only = yes"
-sudo postconf -e "smtpd_tls_cert_file = /etc/ssl/certs/$hostname.crt"
-sudo postconf -e "smtpd_tls_key_file = /etc/ssl/private/$hostname.key"
-sudo postconf -e "smtpd_tls_loglevel = 1"
-sudo postconf -e "smtpd_tls_security_level = may"
+# Configure Postfix to use the SSL certificate
+sudo postconf -e "smtpd_tls_cert_file = /etc/ssl/certs/postfix.crt"
+sudo postconf -e "smtpd_tls_key_file = /etc/ssl/private/postfix.key"
+sudo postconf -e "smtp_tls_cert_file = /etc/ssl/certs/postfix.crt"
+sudo postconf -e "smtp_tls_key_file = /etc/ssl/private/postfix.key"
 sudo postconf -e "smtpd_use_tls = yes"
-sudo postconf -e "virtual_alias_maps = hash:/etc/postfix/virtual"
-sudo postconf -e "header_checks = regexp:/etc/postfix/header_checks"
+sudo postconf -e "smtp_use_tls = yes"
 
-# Create virtual alias map
-sudo touch /etc/postfix/virtual
-sudo echo "$mailuser@$hostname $mailuser" >> /etc/postfix/virtual
-sudo postmap /etc/postfix/virtual
+# Configure Postfix to use the provided email address and password
+sudo postconf -e "relayhost = [$email]:587"
+sudo postconf -e "smtp_sasl_auth_enable = yes"
+sudo postconf -e "smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd"
+sudo postconf -e "smtp_sasl_security_options = noanonymous"
+echo "[$email]:587 $email:$password" | sudo tee -a /etc/postfix/sasl_passwd
+sudo postmap /etc/postfix/sasl_passwd
 
-# Create header_checks file
-sudo touch /etc/postfix/header_checks
-sudo echo "/^Received:/ IGNORE" >> /etc/postfix/header_checks
+# Turn off the outgoing Received header
+sudo postconf -e "disable_vrfy_command = yes"
+sudo postconf -e "smtpd_discard_ehlo_keyword_address_maps = hash:/etc/postfix/discard"
+echo "received" | sudo tee -a /etc/postfix/discard
+sudo postmap /etc/postfix/discard
 
-# Restart postfix
+# Restart Postfix to apply the changes
 sudo service postfix restart
