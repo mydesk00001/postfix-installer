b9c5e737691a2a38afcd9d1ce1de864c33cf0199
mydesk00001 <114636614+mydesk00001@users.noreply.github.com>
2023-02-11 18:38:41 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update the package list
-sudo apt-get update -y
+sudo apt-get update
 
 # Install Postfix and OpenSSL
-sudo apt-get install postfix openssl -y
+sudo apt-get install postfix openssl
 
 # Prompt the user for the email address, password, and hostname
 read -p "Enter your email address: " email
@@ -34,7 +34,7 @@ sudo postmap /etc/postfix/sasl_passwd
 # Turn off the outgoing Received header
 sudo postconf -e "disable_vrfy_command = yes"
 sudo postconf -e "smtpd_discard_ehlo_keyword_address_maps = hash:/etc/postfix/discard"
-echo "received discard_it" | sudo tee -a /etc/postfix/discard
+echo "received discard: discard_it" | sudo tee -a /etc/postfix/discard
 sudo postmap /etc/postfix/discard
 
 # Restart Postfix to apply the changes
